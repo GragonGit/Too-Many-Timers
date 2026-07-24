67bec7600e2b96ba7879d7cd1f897556e18b2ede
Gragon <gragonator02@gmail.com>
2026-07-24 17:33:23 +0200

    Added Score logic

--- UI/ScoreDisplay/ScoreScript.gd
+++ UI/ScoreDisplay/ScoreScript.gd
@@ -1,8 +1,29 @@
-extends Label
+extends Node2D
+
+@export var digits_texture: CompressedTexture2D
+@export var digit_size: Vector2i = Vector2i(3, 5)
+@export var digit_spacing: int = 2
+@export var min_digits: int = 10
+
+var digits: Array[int] = []
 
 func _ready() -> void:
 	ScoreManager.score_changed.connect(_on_score_changed)
-	text = "Score: " + str(ScoreManager.score)
+	_on_score_changed(ScoreManager.score)
 
-func _on_score_changed(new_score: int) -> void:
-	text = "Score: " + str(new_score)
+func _on_score_changed(value: int) -> void:
+	digits.clear()
+	var v: int = maxi(value, 0)
+	while v > 0 or digits.size() < 1:
+		digits.append(v % 10)
+		v /= 10
+	while digits.size() < min_digits:
+		digits.append(0)
+	queue_redraw()
+
+func _draw() -> void:
+	var step: int = digit_size.x + digit_spacing
+	for i in digits.size():
+		var src := Rect2(Vector2(digits[i] * digit_size.x, 0), Vector2(digit_size))
+		var dst := Rect2(Vector2(1 - i * step, -1), Vector2(digit_size))
+		draw_texture_rect_region(digits_texture, dst, src)
